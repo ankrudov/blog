1ab8e33e5b73f2a409dc9fc9e731d819c1945e8d
andre <78390294+ankrudov@users.noreply.github.com>
2021-09-18 14:11:42 -0500

    finished with serializers and made migrations. adding views for the API

--- blog/models.py
+++ blog/models.py
@@ -8,24 +8,25 @@ class Categories(models.TextChoices):
     LIFE = 'life'
     SUSTAINABILITY = 'sustainability'
 
-class BlogPost(models.model):
+class BlogPost(models.Model):
     title = models.CharField(max_length=50)
-    slug = models.SlugField()
+    slug = models.SlugField() #unique identifier
     thumbnail = models.ImageField(upload_to='photos/%Y/%m/%d/')
     category = models.CharField(max_length=50,choices=Categories.choices, default=Categories.LIFE)
-    description = models.CharField(max_length=150)
+    description = models.CharField(max_length=150) #excerpt
     month= models.CharField(max_length=3)
     day= models.CharField(max_length=2)
-    content = models.TextField()
+    content = models.TextField() #inheriting from django summernote
     featured = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.now,blank=True)
     
+    #saving posts
     def save(self,*args,**kwargs):
-        original_slug = slugify(self.title)
-        queryset = BlogPost.objects.all().filter(slug__iexact=original_slug).count()
+        original_slug = slugify(self.title) #slug = title-like-this
+        queryset = BlogPost.objects.all().filter(slug__iexact=original_slug).count() #counts slugs that match the original slug
 
-        count = 1 
-        slug = original_slug
+        count = 1 #will be used for indexing the urls
+        slug = original_slug #sets slug to slugified title
 
         #checks if posts are unique
         while(queryset):
